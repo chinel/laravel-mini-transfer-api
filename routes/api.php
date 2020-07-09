38907b032669e008b9e17de594b11e8e0a62a30d
nelo <nelokwaz@gmail.com>
2020-07-09 11:19:08 +0100

    created the routes needed

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', 'ApiController@login');
+Route::post('register', 'ApiController@register');
+Route::group(['middleware' => 'auth.jwt'], function () {
+    Route::get('logout', 'ApiController@logout');
+    Route::get('user', 'ApiController@getAuthUser');
+    Route::post('topUpAccount', 'TransactionController@topUp');
+    Route::get('checkAccountBalance', 'TransactionController@accountBalance');
+    Route::post('transferFunds', 'TransactionController@transferFunds');
+    Route::get('transactionHistory', 'TransactionController@history');
 });
